f457f2b51fd7186e26ed695aa3795e74dcdeb10c
Eyasu Araya <h00396248@hct.ac.ae>
2023-06-27 15:00:11 +0400

    programmed javascript file for playing RPS game with computer

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice(){
     let opt1 =[1,2,3]
     let ind=Math.floor(Math.random()*opt1.length)
@@ -15,55 +17,55 @@ function getComputerChoice(){
     
     function playRound(player,computer){
         let con;
-        if (player.toLowerCase()==="rock" && computer==="Rock"){
+        if (player==="rock" && computer==="Rock"){
         console.log("its a tie try again")
         return 0
         }
-        else if (player.toLowerCase()==="rock" && computer==="Paper"){
+        else if (player==="rock" && computer==="Paper"){
         console.log("You lost Paper beats rock")
         return 2
         }
-        else if (player.toLowerCase()==="rock" && computer==="Scissor"){
+        else if (player==="rock" && computer==="Scissor"){
         console.log("You Win Rock beats Scissors")
         return 1
         }
-        else if (player.toLowerCase()==="paper" && computer==="Rock"){
+        else if (player==="paper" && computer==="Rock"){
         console.log("You Win "+player+" beats "+computer+". ")
         return 1
         }
-        else if (player.toLowerCase()==="paper" && computer==="Paper"){
+        else if (player==="paper" && computer==="Paper"){
         console.log("its a tie try again")
         return 0
         }
-        else if (player.toLowerCase()==="paper" && computer==="Scissor"){
+        else if (player==="paper" && computer==="Scissor"){
         console.log("You lost Scissors beats Paper")
         return 2
         }
-        else if (player.toLowerCase()==="scissor" && computer==="Rock"){
+        else if (player==="scissor" && computer==="Rock"){
         console.log("You lost Rock beats Scissors")
         return 2
         }
-        else if (player.toLowerCase()==="scissor" && computer==="Paper"){
+        else if (player==="scissor" && computer==="Paper"){
         console.log("You Win Scissors beats Paper")
         return 1
         }
-        else if (player.toLowerCase()==="scissor" && computer==="Scissor"){
+        else if (player==="scissor" && computer==="Scissor"){
         console.log("its a tie try again")
         return 0
-        return 
+        
         }
         else{
-        console.log("You put undefined input so you lose by default")
+        console.log("You put undefined input so you lose by default "+ player)
         return 2
         }
     }
     
     /*console.log(playRound("rock",getComputerChoice()))*/
-    function game(){
+    function game(playersel){
         let counC=0;
         let counP=0;
         for (let i=0; i<5;i++){
-            let playersel=prompt('chose paper,rock or scissors')
+            //let playersel=prompt('chose paper,rock or scissors')
             let round =playRound(playersel,getComputerChoice())
             if(round===1){
             counP++
@@ -80,3 +82,49 @@ function getComputerChoice(){
             return "You lost: computer "+counC+" YOU "+counP
         }
     }//console.log(game())
+let scop=0;
+let scoc=0;
+window.addEventListener('click',function(e){
+    if(e.target.className === 'paper' || e.target.className==='rock' ||e.target.className==='scissor'){
+        let winner= playRound(e.target.className,getComputerChoice())
+        const win =document.querySelector('.footer')
+        const scorep =document.querySelector('.playerSCO')
+        const scorec =document.querySelector('.computerSCO')
+        
+        if (winner===1){
+            
+            win.textContent='You win'
+            scop=scop+1;
+            scorep.textContent='score = '+scop
+
+        }else if(winner===2){
+            
+            win.textContent='You lose'
+            scoc=scoc+1;
+            scorec.textContent='score = '+scoc
+        }else if(winner===0){
+            win.textContent="it's a tie"
+        }
+        if(scop>=5 ){
+            scop=0;
+            scoc=0;
+            win.textContent="congratulations, You are the overall winner"
+            scorec.textContent='score = 0'
+            scorep.textContent='score = 0'
+        }else if(scoc>=5){
+            scop=0;
+            scoc=0;
+            win.textContent="Sorry, you are the overall losser. try again"
+            scorec.textContent='score = 0'
+            scorep.textContent='score = 0'
+        }
+    }
+   
+    
+
+
+})
+
+
+    
+    
